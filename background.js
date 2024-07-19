4ccfcdd68183a3f96dd6577ab364b8d08a5ad2c3
Joff Tiquez <jofftiquez@gmail.com>
2024-07-19 20:04:46 +0800

    chore: Refactor background.js to use arrow function syntax
    Refactor the code in background.js to use arrow function syntax for the event listeners. This improves code readability and follows modern JavaScript conventions.

--- background.js
+++ background.js
@@ -1,16 +1,6 @@
-chrome.webRequest.onHeadersReceived.addListener(
-  function(details) {
-    const statusCode = details.statusCode;
-    if (statusCode === 429) {
-      chrome.runtime.sendMessage({ error: "Rate limit exceeded" });
-    }
-  },
-  { urls: ["https://www.strava.com/*"] },
-  ["responseHeaders"]
-);
-
-chrome.runtime.onMessage.addListener(function(message) {
-  if (message.error) {
-    alert(message.error);
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "startKudosClicker") {
+    // startKudosClicker(request.settings);
   }
 });
+
